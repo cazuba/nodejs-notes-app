7fcb8ead423f3d4759bd30da430698ea18622002
cazuba <krlos2290@gmail.com>
2018-10-25 14:32:48 -0600

    Playground 2

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 console.log('App initialized...');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes');
 
-const user = os.userInfo();
-
-// Lodash samples
-console.log(_.isString(true));
-console.log(_.isString('Charlie'));
-
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
-    if(err) {
-        throw err;
-    }
-    fs.readFile('greetings.txt', (err, data) => {
-        if(err) {
-            throw err;
-        }
-        console.log(data);
-    });
-});
-
-const filteredArray = _.uniq(['Charlie', 1, 'Charlie', 2, 3, 'CharlieLorem']);
-console.log(filteredArray);
