65dea4caeb9e1e59ec2b6f09568e84b957f6706a
cazuba <krlos2290@gmail.com>
2018-10-25 14:40:02 -0600

    Using command line variables

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes');
 
+const Command = process.argv[2];
+
+switch(Command) {
+    case 'add': 
+        console.log('Adding notes...')
+        break;
+    case 'list': 
+        console.log('Listing notes...')
+        break;
+    case 'read': 
+        console.log('Reading notes...')
+        break;
+    default: 
+        console.log('Command not found...')
+        break;
+}
