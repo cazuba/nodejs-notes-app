7f41cbd5895e2c68806f647059e4c317240af25d
cazuba <krlos2290@gmail.com>
2018-10-25 15:27:26 -0600

    Playground updates

--- playground/json.js
+++ playground/json.js
@@ -1,10 +1,15 @@
-const obj = {
-    name: 'Charlie'
-};
+// const obj = {
+//     name: 'Charlie'
+// };
 
-const stringObj = JSON.stringify(obj);
+// const stringObj = JSON.stringify(obj);
 
-console.log(typeof stringObj, "\n", stringObj);
-module.export = {
-    mock: stringObj
-};
+// console.log(typeof stringObj, "\n", stringObj);
+// module.export = {
+//     mock: stringObj
+// };
+
+const personString = '{"name":"Charlie", "age":25}';
+const person = JSON.parse(personString);
+
+console.log(typeof person, "\n", person);
